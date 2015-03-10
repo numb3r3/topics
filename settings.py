29f69c97f1224c7088990d1fc12bef22162080fc
numb3r3 <314159>
2015-03-10 11:25:39 +0800

    adopt my own settings

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ class Settings:
     def __init__(self):
         pass
 
-    DATASET_FILE = 'dataset/yelp_dataset_challenge_academic_dataset'
+    DATASET_FILE = '../yelp_nyc'
     MONGO_CONNECTION_STRING = "mongodb://localhost:27030/"
-    REVIEWS_DATABASE = "Dataset_Challenge_Reviews"
+    REVIEWS_DATABASE = "yelp_nyc_restaurants"
     TAGS_DATABASE = "Tags"
     REVIEWS_COLLECTION = "Reviews"
     CORPUS_COLLECTION = "Corpus"
